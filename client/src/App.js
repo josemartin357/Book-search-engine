986f370898d65d6f789492bd2b88c392ed16dcf4
Martin Bedoya <macowner@macs-MBP.attlocal.net>
2022-01-13 23:11:17 -0600

    Re-worked the server.js setup and dependencies. Installed apollo-server graphql

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -10,9 +10,9 @@ function App() {
       <>
         <Navbar />
         <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          <Route exact path="/" component={SearchBooks} />
+          <Route exact path="/saved" component={SavedBooks} />
+          <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
         </Switch>
       </>
     </Router>
--- server/server.js
+++ server/server.js
@@ -1,23 +1,24 @@
-// ADJUSTED FOR GRAPHQL AND APOLLO
+// NOTES: needed to install: npm install apollo-server graphql
+// TO RUN: npm from the root folder, then npm run develop
+
 const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
 const routes = require("./routes");
-// requiring Apollo Server
+// Requiring Apollo Server
 const { ApolloServer } = require("apollo-server-express");
+// Requiring schemas: typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
-const { authMiddleware } = require("./utils/auth");
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Setting up Apollo Server
+// Setting-up ApolloServer
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
 });
 
+// Setting-up middleware
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
@@ -28,14 +29,15 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-// app.use(routes);
+// Since the React front-end application will handle its own routing, we set up a wildcard route on our server that will serve the front end whenever a request for a non-API route is received.
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 db.once("open", () => {
   app.listen(PORT, () => {
-    console.log(`Now listening on localhost:${PORT}`);
+    console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
