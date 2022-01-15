b166a8900de20d3afa7a0b52418a927367ee79ba
Martin Bedoya <macowner@macs-MBP.attlocal.net>
2022-01-14 22:58:39 -0600

    Added an input bookInput in typeDefs. mutations.js: Added queries for mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,66 @@
-// LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
+import { gql } from "@apollo/client";
 
-// ADD_USER will execute the addUser mutation
+// LOGIN_USER will execute the loginUser mutation set up using Apollo Server; which at the same time will pass a token
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
 
-// SAVE_BOOK will execute the saveBook mutation.
+// ADD_USER will execute the addUser mutation and will also pass a token
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// SAVE_BOOK will execute the saveBook mutation. In this case, it also references to the User
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: bookInput!) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
 
 // REMOVE_BOOK will execute the removeBook mutation.
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -23,21 +23,25 @@ const typeDefs = gql`
     user(userId: ID!, username: String!): User
     me: User
   }
+
   type Auth {
     token: ID
     user: User
   }
+
+  input bookInput {
+    bookId: String!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+  }
+
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(
-      bookId: String!
-      authors: [String]!
-      description: String
-      image: String
-      link: String
-      title: String!
-    ): User
+    saveBook(input: bookInput): User
     removeBook(bookId: String!): User
   }
 `;
